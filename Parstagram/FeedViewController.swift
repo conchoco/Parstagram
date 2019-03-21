47995197d410c1e8ec9919f1e5cad5a5de2104a7
user148140 <user148140@ny011.local>
2019-03-21 13:46:47 -0700

    Initial Commit

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,100 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by user148140 on 3/20/19.
+//  Copyright © 2019 user148140. All rights reserved.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    var refreshControl: UIRefreshControl! // Variable for refresh control
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+        
+        // Refresh fucntion added to tableView
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+    }
+    
+    // Refresh method
+    @objc func onRefresh() {
+        self.refreshControl.endRefreshing()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // Delay method to simulate loading
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
+    }
+    
+    func refresh() {
+        run(after: 2) {
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+
+}
---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by user148140 on 3/20/19.
+//  Copyright © 2019 user148140. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        // Sign-up code block copied from Parse iOS API
+        let user = PFUser() // Changed var to let
+        user.username = usernameField.text // Use value set in the outlet
+        user.password = passwordField.text // Use value set in the outlet
+        // user.email = "email@example.com" - Not using
+        // other fields can be set just like with PFObject
+        // user["phone"] = "415-392-0202" - Not using
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        } // Added manually
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
